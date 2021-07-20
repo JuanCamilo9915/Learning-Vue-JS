e320b64b134737db01a8e36eab1f7c748b20dfa0
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-07-20 00:11:38 -0500

    Se implementan los eventos, en este caso de tipo Click con Vue JS, donde trabaja con la propiedad methods con la finalidad de pasar datos de un input y ser mostrados en una lista, en este caso se agrega únicamente el nombre del fabricante de aviones.

--- Aprendizaje-Guia/proyect-00/codigo-vue.js
+++ Aprendizaje-Guia/proyect-00/codigo-vue.js
@@ -1,12 +1,25 @@
 const appVue = new Vue({
-    el: '#app-vue-js',
-    data: {
+    el: '#app-vue-js',//"el" es una palabra reservada de vue JS
+    data: {//"data" es una palabra reservada de vue JS
         titulo: 'Hellow Vue JS',
-        frutas: ['Mango','Pera',"Manzana"],
-        carros: ['Onix RS','Polo','Escape','Alto 800'],
+        frutas: ['Mango', 'Pera', "Manzana"],
+        carros: ['Onix RS', 'Polo', 'Escape', 'Alto 800'],
         aviones: [
-            {empresa: 'Boing', flota: [787,737,747]},
-            {empresa: 'Airbus', flota: [350,320,380]}
-        ]
+            { empresa: 'Boing', flota: [787, 737, 747] },
+            { empresa: 'Airbus', flota: [350, 320, 380] }
+        ],
+        nuevofabricante: ''
+    },
+    methods: {
+        prueba: function(){
+            alert('Esto es la forma antigua de como se podia utilizar un método en Vue JS');
+        },
+        agregarFabricanteAviones () {
+            /*console.log("Se agrego el precio al producto");
+            alert('Se agrego el precio al producto');*/
+            this.aviones.push({
+                empresa: this.nuevofabricante
+            })
+        }
     }
 });
