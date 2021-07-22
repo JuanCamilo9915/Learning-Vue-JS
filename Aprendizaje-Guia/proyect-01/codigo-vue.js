91eda4c74596c64c8fef04c54a046e4c96d4cbbf
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-07-21 23:46:49 -0500

    se agrega el proyecto de gestión de  ejercicios para el Gym, al proyecto 2, donde se crearon 2 instancias cada una para cada proyecto.

---
+++ Aprendizaje-Guia/proyect-01/codigo-vue.js
@@ -0,0 +1,20 @@
+const appVue = new Vue({
+    el: '#appVue',
+    data: {
+        fondo: 'bg-success',
+        color: false
+    },
+    methods: {
+
+    }
+});
+/*-------------------------------------------*/
+const appGym = new Vue({
+    el: '#appGym',
+    data: {
+        titulo: 'Gestión de Ejercicios del Gym'
+    },
+    methods: {
+
+    }
+});
